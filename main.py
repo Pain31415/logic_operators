98093e74fb0b73981ddc0cf639f312ae34ff49b2
Pain31415 <Cherniy412@ukr.net>
2023-09-17 11:48:20 +0200

    lab4

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 a = int(input())
-b = int(input())
-c = int(input())
-if a % 2 == 0 or b % 2 == 0 or c % 2 == 0:
-    if a % 2 == 1 or b % 2 == 1 or c % 2 == 1:
-        print("Even number")
-    else:
-        print("Odd number")
+if a % 7 ==0:
+    print('Number is a multiple of ')
 else:
-    print("NO")
+    print('Number is not a multiple of 7')
