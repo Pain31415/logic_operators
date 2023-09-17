41b2b73ed12a36451201db29d59022f2121cd5a0
Pain31415 <Cherniy412@ukr.net>
2023-09-17 11:48:20 +0200

    hm1

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 a = int(input())
 b = int(input())
 c = int(input())
-if a % 2 == 0 or b % 2 == 0 or c % 2 == 0:
-    if a % 2 == 1 or b % 2 == 1 or c % 2 == 1:
-        print("Even number")
-    else:
-        print("Odd number")
-else:
-    print("NO")
+print(a+b+c)
+print(a*b*c)
